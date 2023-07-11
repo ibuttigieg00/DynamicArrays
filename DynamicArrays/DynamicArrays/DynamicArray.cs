7b68e6608b4af4cead6131d63a880404f7480d7a
izaak buttigieg <izaakbuttigieg00@gmail.com>
2023-07-11 11:29:55 +0200

    Started and finished Dynamic arrays
    Implemented functionalities to PushBack in the array and to double the size of the array whenever it is full. Time complexity is O(1) in the insertion (push-back) and O(N) in the resizing.

---
+++ DynamicArrays/DynamicArrays/DynamicArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrays
+{
+    public class DynamicArray
+    {
+        int[] numbers;
+        public int length { get; set; }
+        private int counter = 0;
+        public DynamicArray(int _length)
+        {
+            length = _length;
+            numbers = new int[length];
+        }
+
+        // Insert n in the last position of the array
+        public void PushBack(int n)
+        {
+            if (length == counter)
+            {
+                Resize();
+                length = numbers.Length;
+            }
+            numbers[counter] = n;
+            counter++;
+        }
+
+        public void Resize()
+        {
+            int[] resizedArray = new int[2*length];
+
+            for (int i = 0; i < length; i++)
+            {
+                resizedArray[i] = numbers[i];
+            }
+            numbers = resizedArray;
+        }
+    }
+}
---
+++ DynamicArrays/DynamicArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using DynamicArrays;
+
+class Program
+{
+    public static void Main(string[] args )
+    {
+        DynamicArray da = new DynamicArray(3);
+
+        bool WantToContinue = true;
+        string input = "";
+        int number = 0;
+
+        while (WantToContinue)
+        {
+            Console.WriteLine("Do you wish to continue?? Y or N ");
+            input = Console.ReadLine();
+
+            if (input == "Y" || input == "y")
+            {
+                Console.WriteLine("Enter a number to enter in the array: ");
+                number = int.Parse(Console.ReadLine());
+                da.PushBack(number);
+            }
+            else
+                WantToContinue = false;
+        }
+        Console.ReadKey();
+    }
+}
